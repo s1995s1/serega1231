412ce9525aca295652f4f927fc64f1034c729b20
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2023-04-07 21:44:55 +0300

    велг

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -132,8 +132,8 @@ let vn = './media/тост.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
     
-if (!chat.isBanned && chat.audios && m.text.match(/(удальть)/gi)) {    
-let vn = './media/удальть.mp3'
+if (!chat.isBanned && chat.audios && m.text.match(/(удалить)/gi)) {    
+let vn = './media/удалить.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
     
